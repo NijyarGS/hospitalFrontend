66625e9726a6a76a8b7e45f357dfa099f4140bb0
Nijyar Ghazi <nijyar360@gmail.com>
2025-02-09 16:13:44 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+
+function App() {
+  return <p className="h1">hello</p>;
+}
+
+export default App;
+
+/* 
+
+Patient's Chart
+-------------------
+
+Stage 1: Data Structure & UI Design
+
+- Gather Data about how a patient chart is structured
+- Search for Medial UI designs for patient charts
+
+Stage 2 : Data 
+- create Simple mockup data structure with a mockup api
+
+Stage 3: UI Design Figma (PHONE CENTRIC)
+- create Login Page
+- Create Patient Chart general Page layout
+- Create Patient Chart List
+- Create Patient Chart filter design
+- Create Patient Chart edit modal design
+- Create Patient Chart add cases modal design
+
+Stage 4: Code
+- Code.
+
+
+
+Day 1 : Login Page & Cases mockup Data & Cases List Design.
+Day 2 : Cases List Implementation & Filter Design and implementation.
+Day 3 : Add & Edit Cases Modal Design & Implementation.
+
+*/
