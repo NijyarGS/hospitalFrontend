77ac410df6f469cf34684179713352cbadf3ecf2
Nijyar Ghazi <nijyar360@gmail.com>
2025-02-10 22:22:38 +0300

    added i18next and arabic language support

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
+import "./i18n";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,36 @@
+import { useTranslation } from "react-i18next";
+import i18next from "i18next";
+
 export default function Login() {
+  const { t } = useTranslation();
+  const dir = i18next.dir();
+
   return (
-    <div className="d-flex justify-content-center align-items-center h-100">
+    <div
+      dir={dir}
+      className="d-flex justify-content-center align-items-center h-100"
+    >
       <div
-        className="bg-body-tertiary border border-1 p-sm-5 p-3 m-sm-0 m-4"
+        className="bg-body-tertiary border border-1 p-sm-5 p-4 m-sm-0 m-4"
         style={{ width: "max-content" }}
       >
         <div className="w-100" style={{ maxWidth: "400px" }}>
-          <h2 className="h1 fw-bold mb-5 pb-3">Login</h2>
+          <h2 className="h1 fw-bold mb-5 pb-3">{t("login")}</h2>
           <div className="mb-5 pb-3">
             <div className="form-group mb-3 pb-3">
-              <label className="mb-3">Username:</label>
+              <label className="mb-3">{t("username")} :</label>
               <input type="text" className="form-control rounded-0" />
             </div>
             <div className="form-group mb-3 pb-3">
-              <label className="mb-3">Password:</label>
+              <label className="mb-3">{t("password")} :</label>
               <input type="password" className="form-control rounded-0" />
             </div>
-            <button className="btn btn-primary w-100 rounded-0">Login</button>
+            <button className="btn btn-primary w-100 rounded-0">
+              {t("submit")}
+            </button>
           </div>
 
-          <p className="mb-0 small text-muted">
-            Contact Supervisor in the case of forgetting your password{" "}
-          </p>
+          <p className="mb-0 small text-muted">{t("contact")}</p>
         </div>
       </div>
     </div>
