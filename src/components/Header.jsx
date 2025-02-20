227993db04984d20fbc89a04fb6118dcd8e80e91
Nijyar Ghazi <nijyar360@gmail.com>
2025-02-20 05:55:08 +0300

    added mobile sidebar added responsive colapsable desktop sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,21 @@
-export default function Header() {
+import i18next from "i18next";
+
+/* eslint-disable react/prop-types */
+export default function Header({ toggleSidebar, isSidebarDesktopOpen }) {
+  const dir = i18next.dir();
+
   return (
     <header className="bg-body p-3 container-fluid border-bottom">
       <section className="d-flex justify-content-between align-items-center">
         <div>
-          <a className="p-0 link-primary h4" style={{ cursor: "pointer" }}>
-            <i className="bi bi-layout-sidebar" />
-            {
-              //  <i className="bi bi-layout-sidebar-reverse"/>
-              // <i className="bi bi-layout-sidebar-inset"/>
-              // <i className="bi bi-layout-sidebar-inset-reverse"/>
-            }
+          <a
+            className={`p-0 h4 ${
+              isSidebarDesktopOpen ? "link-body-emphesis" : "link-primary"
+            }`}
+            style={{ cursor: "pointer" }}
+            onClick={toggleSidebar}
+          >
+            <MenuIcon isSidebarDesktopOpen={isSidebarDesktopOpen} dir={dir} />
           </a>
         </div>
 
@@ -25,3 +31,16 @@ export default function Header() {
     </header>
   );
 }
+
+const MenuIcon = ({ dir, isSidebarDesktopOpen }) =>
+  dir === "rtl" ? (
+    isSidebarDesktopOpen ? (
+      <i className="bi bi-layout-sidebar-reverse" />
+    ) : (
+      <i className="bi bi-layout-sidebar-inset-reverse" />
+    )
+  ) : isSidebarDesktopOpen ? (
+    <i className="bi bi-layout-sidebar" />
+  ) : (
+    <i className="bi bi-layout-sidebar-inset" />
+  );
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,21 +2,44 @@ import i18next from "i18next";
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import { useMediaQuery } from "react-responsive";
 
 export default function MainLayout() {
-  const sidebarWidth = "280px";
-  // const sidebarWidth = "auto";
+  const [isSidebarDesktopOpen, setIsSidebarDesktopOpen] = useState(true);
+  const [isSidebarMobileOpen, setIsSidebarMobileOpen] = useState(false);
+  const isMobile = useMediaQuery({ maxWidth: 767 });
 
+  const sidebarWidth = isSidebarDesktopOpen ? "280px" : "65px";
+  // const sidebarWidth = "auto";
   const dir = i18next.dir();
+
+  function toggleSidebar() {
+    isMobile
+      ? setIsSidebarMobileOpen((prev) => !prev)
+      : setIsSidebarDesktopOpen((prev) => !prev);
+  }
 
   return (
     <div
       className="h-100 d-flex flex-column justify-content-stretch bg-body-tertiary"
       dir={dir}
     >
-      <Sidebar sidebarWidth={sidebarWidth} />
-      <div className="d-flex flex-column h-100 custom_margin_start">
-        <Header />
+      <Sidebar
+        sidebarWidth={sidebarWidth}
+        isMobile={isMobile}
+        isSidebarDesktopOpen={isSidebarDesktopOpen}
+        isSidebarMobileOpen={isSidebarMobileOpen}
+        toggleSidebar={toggleSidebar}
+      />
+      <div
+        className="d-flex flex-column h-100"
+        style={{ marginInlineStart: sidebarWidth }}
+      >
+        <Header
+          toggleSidebar={toggleSidebar}
+          isSidebarDesktopOpen={isSidebarDesktopOpen}
+        />
         <main className="p-md-4 p-3 h-100">
           <Outlet />
         </main>
